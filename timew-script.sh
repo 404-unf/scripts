8a7ff7363105453579ddfd8baa42de41bd8260de
terra9 <avinash.r.kanaujiya@gmail.com>
2020-09-12 22:28:32 +0530

    optimzing the script

--- timew-script.sh
+++ timew-script.sh
@@ -8,10 +8,10 @@ waittime="2"
 # second argument: glyph which forms the border
 # third argument: width of the padding
 if [ "$TERM" = "xterm-256color" ]; then
-  xprop \
-    -id $(xdotool getactivewindow) \
-    -f _MOTIF_WM_HINTS 32c \
-    -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
+	xprop \
+	-id $(xdotool getactivewindow) \
+	-f _MOTIF_WM_HINTS 32c \
+	-set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
 fi
 
 #if (get_window_class()=="scripts") then
@@ -51,46 +51,50 @@ COLOR='\e[0m'
 center_text()
 {
 
-    local terminal_width=$(tput cols)     # query the Terminfo database: number of columns
-    local text="${1:?}"                   # text to center
-    local glyph="${2:- }"                 # glyph to compose the border
-    local padding="${3:-2}"               # spacing around the text
+	local terminal_width=$(tput cols)     # query the Terminfo database: number of columns
+	local text="${1:?}"                   # text to center
+	local glyph="${2:- }"                 # glyph to compose the border
+	local padding="${3:-2}"               # spacing around the text
 
-    local text_width=${#text}
+	local text_width=${#text}
 
-    local border_width=$(( (terminal_width - (padding * 2) - text_width) / 2 ))
+	local border_width=$(( (terminal_width - (padding * 2) - text_width) / 2 ))
 
-    local border=                         # shape of the border
+	local border=                         # shape of the border
 
-    # create the border (left side or right side)
-    for ((i=0; i<border_width; i++))
-    do
-        border+="${glyph}"
-    done
+	# create the border (left side or right side)
+	for ((i=0; i<border_width; i++))
+	do
+		border+="${glyph}"
+	done
 
-    # a side of the border may be longer (e.g. the right border)
-    if (( ( terminal_width - ( padding * 2 ) - text_width ) % 2 == 0 ))
-    then
-        # the left and right borders have the same width
-        local left_border=$border
-        local right_border=$left_border
-    else
-        # the right border has one more character than the left border
-        # the text is aligned leftmost
-        local left_border=$border
-        local right_border="${border}${glyph}"
-    fi
+	# a side of the border may be longer (e.g. the right border)
+	if (( ( terminal_width - ( padding * 2 ) - text_width ) % 2 == 0 ))
+	then
+		# the left and right borders have the same width
+		local left_border=$border
+		local right_border=$left_border
+	else
+		# the right border has one more character than the left border
+		# the text is aligned leftmost
+		local left_border=$border
+		local right_border="${border}${glyph}"
+	fi
 
-    # space between the text and borders
-    local spacing=
+	if (( (left_border - right_border) == 2))
+	then
+		printf "IT Works"
+	fi
+	# space between the text and borders
+	local spacing=
+	local spacings="  "
+	for ((i=0; i<$padding; i++))
+	do
+		spacing+=" "
+	done
 
-    for ((i=0; i<$padding; i++))
-    do
-        spacing+=" "
-    done
-
-    # displays the text in the center of the screen, surrounded by borders.
-    printf "${left_border}${spacing}${text}${spacing}${right_border}\n"
+	# displays the text in the center of the screen, surrounded by borders.
+	printf "${spacings}${left_border}${spacing}${text}${spacing}${right_border}\n"
 }
 
 # center_text "Something I want to print" "~"

a278d8a23a06b0762da6efdc789e8269a928a4d0
terra9 <avinash.r.kanaujiya@gmail.com>
2020-09-12 17:11:57 +0530

    add color codes & borderless tweaks

--- tw-auto-script.sh
+++ tw-auto-script.sh
@@ -7,6 +7,46 @@ waittime="2"
 # first argument: text to center
 # second argument: glyph which forms the border
 # third argument: width of the padding
+if [ "$TERM" = "xterm-256color" ]; then
+  xprop \
+    -id $(xdotool getactivewindow) \
+    -f _MOTIF_WM_HINTS 32c \
+    -set _MOTIF_WM_HINTS "0x2, 0x0, 0x0, 0x0, 0x0"
+fi
+
+#if (get_window_class()=="scripts") then
+#   set_opacity(0.95);
+#fi
+
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+COLOR_NC='\e[0m' # No Color
+COLOR_WHITE='\e[1;37m'
+COLOR_BLACK='\e[0;30m'
+COLOR_BLUE='\e[0;34m'
+COLOR_LIGHT_BLUE='\e[1;34m'
+COLOR_GREEN='\e[0;32m'
+COLOR_LIGHT_GREEN='\e[1;32m'
+COLOR_CYAN='\e[0;36m'
+COLOR_LIGHT_CYAN='\e[1;36m'
+COLOR_RED='\e[0;31m'
+COLOR_LIGHT_RED='\e[1;31m'
+COLOR_PURPLE='\e[0;35m'
+COLOR_LIGHT_PURPLE='\e[1;35m'
+COLOR_BROWN='\e[0;33m'
+COLOR_YELLOW='\e[1;33m'
+COLOR_GRAY='\e[0;30m'
+COLOR_LIGHT_GRAY='\e[0;37m'
+
+COLOR='\e[0m'
 
 center_text()
 {
@@ -58,8 +98,9 @@ center_text()
 while true
 do
 	clear
+	#setcolord
 	timew > tw.txt
-	center_text "$(head -n 1 tw.txt)"
+	center_text "$COLOR$(head -n 1 tw.txt)"
 	rm -r tw.txt
 	sleep "$waittime"
 done

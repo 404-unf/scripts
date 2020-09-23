a28b96d6d914bea1f9ac73599b0ede5f908e9a0c
404-unf <avinash.r.kanaujiya@gmail.com>
2020-09-23 05:56:39 +0530

    replace cp with rclone and reduce the sleep time

--- backup/data-backup-script.sh
+++ backup/data-backup-script.sh
@@ -3,55 +3,57 @@ RED='\033[01;31m'
 NOCOLOR='\033[00m'
 GREEN='\033[01;32m'
 echo "$RED Removing the old .zshrc from local backup directory...$NOCOLOR"
-#rm -r /home/terra9/Storage/backup/zshrc
+rm -r /home/terra9/Storage/backup/zshrc
 echo "$GREEN Creating the directory zshrc..$NOCOLOR"
-#cd /home/terra9/Storage/backup
-#mkdir zshrc
+cd /home/terra9/Storage/backup
+mkdir zshrc
 echo "$GREEN Started copying .zshrc...$NOCOLOR"
-#cp -r /home/terra9/.zshrc /home/terra9/Storage/backup/zshrc/.zshrc
+rclone copy -P /home/terra9/.zshrc /home/terra9/Storage/backup/zshrc/
 echo "$GREEN Done copying .zshrc.$NOCOLOR"
 echo ""
 echo ""
-sleep 2
+sleep .5
 echo "$RED Removing the old .timewarrior from local backup directory...$NOCOLOR"
-#rm -r /home/terra9/Storage/backup/timewarrior
+rm -r /home/terra9/Storage/backup/timewarrior
 echo "$GREEN Creating the directory timewarrior...$NOCOLOR"
-#cd /home/terra9/Storage/backup
-#mkdir timewarrior
+cd /home/terra9/Storage/backup
+mkdir timewarrior && cd timewarrior
+mkdir .timewarrior
 echo "$GREEN Started copying .timewarrior...$NOCOLOR"
-#cp -r /home/terra9/.timewarrior /home/terra9/Storage/backup/timewarrior/.timewarrior
+rclone copy -P /home/terra9/.timewarrior /home/terra9/Storage/backup/timewarrior/.timewarrior
 echo "$GREEN Done copying .timewarrior.$NOCOLOR"
 echo ""
 echo ""
-sleep 2
+sleep .5
 echo "$RED Removing the old .task from local backup directory...$NOCOLOR"
-#rm -r /home/terra9/Storage/backup/taskwarrior
+rm -r /home/terra9/Storage/backup/taskwarrior
 echo "$GREEN Creating the directory taskwarrior$NOCOLOR"
-#cd /home/terra9/Storage/backup
-#mkdir taskwarrior
+cd /home/terra9/Storage/backup
+mkdir taskwarrior && cd taskwarrior
+mkdir .taskwarrior
 echo "$GREEN Started copying .task...$NOCOLOR"
-#cp -r /home/terra9/.task        /home/terra9/Storage/backup/taskwarrior/.task
+rclone copy -P /home/terra9/.task        /home/terra9/Storage/backup/taskwarrior/.taskwarrior
 echo "$GREEN Done copying .task.$NOCOLOR"
 echo ""
 echo ""
-sleep 2
+sleep .5
 echo "$RED Removing the old trilium-data folder from local backup directory...$NOCOLOR"
 rm -r /home/terra9/Storage/backup/trilium-data
 echo "$GREEN Started copying trilium-data...$NOCOLOR"
-cp -r /home/terra9/.local/share/trilium-data /home/terra9/Storage/backup/ && progress -mp $!
+rclone copy /home/terra9/.local/share/trilium-data /home/terra9/Storage/backup/trilium-data -P
 echo "$GREEN Done copying trilium-data.$NOCOLOR"
 echo ""
 echo ""
-sleep 2
+sleep .5
 echo "$GREEN Yay!!! Everything is copied to local backup directory!!!$NOCOLOR"
 echo ""
-sleep 2
+sleep .5
 echo "$GREEN Moving into Local backup directory...$NOCOLOR"
 #cd /home/terra9/Storage/backup
 echo "$GREEN Syncing the remote backup directory with local backup directory on Google Drive... data on remote is being modified...$NOCOLOR"
 #rclone sync . remote:backup -P -L
 echo "$GREEN Done syncing the remote backup directory with local backup directory.$NOCOLOR"
 echo ""
-sleep 2
+sleep .5
 echo ""
 echo "$GREEN Yay!!! Everything is backed up!!!$NOCOLOR"
